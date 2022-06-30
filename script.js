fd6185e4b5c01259eef088003403a890dc2bbb58
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-06-30 09:18:03 -0500

    Added a function that can hold the input of the user and generate a random character.

--- script.js
+++ script.js
@@ -14,32 +14,63 @@ var specialCharacters = ["!", "#", "$", "%", "&", "(", ")", "*", "+", ",", "-", 
 
 function userInput() {
 
-  var pwdLength = window.prompt("Input a number for the length of the password (Between 8 - 128 characters)");
+  var pwdLength = prompt("Input a number for the length of the password (Between 8 - 128 characters)");
+  // Conformation returns true if user selects the following.
   var bigLetters = confirm("Do you want UPPERCASE characters included?");
   var smallLetters = confirm("Do you want LOWERCASE characters included?");
   var uniqueCharacters = confirm("Do you want SPECIAL CHARACTERS included?");
   var userNumbers = confirm("Do you want NUMBERS included?");
 
-
+  // Holds the input in an randomGenerator Object.
   var randomGenerator = {
 
     password: pwdLength,
     upper: bigLetters,
     lower: smallLetters,
-    numbers: userNumbers,
+    num: userNumbers,
     special: uniqueCharacters
 
   }
-  console.log(randomGenerator);
+  // console.log(randomGenerator);
   return randomGenerator;
 
 }
 
 function generatePassword() {
-  var generate = userInput();
+  // userInput is placed in the generator variable and is used in the if statements 
+  var generator = userInput();
+  var megaArray = [];
+  var pwd = "";
+  
+  if(generator.upper){
+    // If the user selects true, the uppercase array will be concatinated into the empty megaArray
+    megaArray = megaArray.concat(uppercase);
+    // The random uppercase length that is generated is placed in the randomUpper variable
+    var randomUpper = [Math.floor(Math.random() * uppercase.length)];
+    // console.log() to verify the random generation of uppercase.
+    console.log(randomUpper);
+  }
 
+  if(generator.lower){
+    megaArray = megaArray.concat(lowercase);
+    var randomLower = [Math.floor(Math.random() * lowercase.length)];
+  }
 
+  if(generator.num){
+    megaArray = megaArray.concat(numbers);
+    var randomNumber =  [Math.floor(Math.random() * numbers.length)];
+  }
 
+  if(generator.special){
+    megaArray = megaArray.concat(specialCharacters);
+    var randomSpecial = [Math.floor(Math.random() * specialCharacters.length)];
+  }
+
+  for(i = 0; i < generator.password; i++){
+    pwd += megaArray[Math.floor(Math.random() * megaArray.length)];
+  }
+  // Generate random password and return the password.
+  return pwd;
 }
 
 // Write password to the #password input
@@ -81,4 +112,4 @@ generateBtn.addEventListener("click", writePassword);
  // How to randomly choose password criteria? Use array methods to combine all arryays into one array.
  // Randomly choose characters
  // Create a string set equal to password var.
-// Generate random password and return the password.
+
